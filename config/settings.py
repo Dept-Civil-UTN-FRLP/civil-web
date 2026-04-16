0cdfd34ccc7c158efcd28ce9df9a3dc1f26c0f54
Fermat <jorge.ronconi@gmail.com>
2026-04-16 15:29:15 -0300

    fix: logout usa POST y redirige a landing

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "/admin/login/"
 LOGIN_REDIRECT_URL = "/" 
+LOGOUT_REDIRECT_URL = "/"
 
 # Email Configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
