fc61b8aac86e4fe0319e5d0ec7ee51f5156ad72e
Fermat <jorge.ronconi@gmail.com>
2026-04-16 15:02:38 -0300

    feat: condicionar URLs y navbar según MODULOS_ACTIVOS

--- config/urls.py
+++ config/urls.py
@@ -21,22 +21,31 @@ from django.contrib import admin
 from django.urls import include, path
 from config.views import landing_civil
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from config.views import landing_civil
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("equivalencia/", include("equivalencias.urls")),
-    path("carrera/", include("carrera_academica.urls")),
-    path("planta/", include("planta_docente.urls")),
-    path("", landing_civil, name="landing")
+    path("", landing_civil, name="landing"),
 ]
 
-# Debug toolbar (solo en desarrollo)
+if "equivalencias" in settings.MODULOS_ACTIVOS:
+    urlpatterns += [path("equivalencia/", include("equivalencias.urls"))]
+
+if "carrera_academica" in settings.MODULOS_ACTIVOS:
+    urlpatterns += [path("carrera/", include("carrera_academica.urls"))]
+
+if "planta_docente" in settings.MODULOS_ACTIVOS:
+    urlpatterns += [path("planta/", include("planta_docente.urls"))]
+
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
+        path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
