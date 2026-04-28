abc141f5d8119a7493113577a3bb61784bc5ac66
Fermat <jorge.ronconi@gmail.com>
2026-04-28 15:46:30 -0300

    fix(context): expose DEPARTAMENTO variable to template context

--- config/context_processors.py
+++ config/context_processors.py
@@ -10,5 +10,6 @@ def modulos_activos(request):
     departamento = settings.DEPARTAMENTO
     return {
         "MODULOS_ACTIVOS": settings.MODULOS_ACTIVOS,
+        "DEPARTAMENTO": departamento,
         "DEPARTAMENTO_LABEL": DEPARTAMENTO_LABELS.get(departamento, departamento.title()),
     }
