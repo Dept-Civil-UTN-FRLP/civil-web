1e3c7691c20cead3c6c8c69fcff914e1163453a2
Fermat <jorge.ronconi@gmail.com>
2026-04-28 18:19:11 -0300

    fix(equivalencias): use filesystem path for firma_imagen in PDF generation

--- equivalencias/views.py
+++ equivalencias/views.py
@@ -49,7 +49,7 @@ def _get_firma_context(request):
             'cargo_firmante': '',
         }
     return {
-        'signature_image_path': config.firma_imagen.path,
+        'signature_image_path': f"file://{config.firma_imagen.path}",
         'nombre_firmante': config.nombre_firmante,
         'cargo_firmante': config.cargo_firmante,
     }
